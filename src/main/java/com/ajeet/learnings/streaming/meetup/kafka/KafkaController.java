b52c378ec9ede38fba88721debe376514d3c42a6
Ajeet Singh <ajeetsinghtomer@gmail.com>
2019-07-15 13:48:12 +0530

    Initial check-in

---
+++ src/main/java/com/ajeet/learnings/streaming/meetup/kafka/KafkaController.java
@@ -0,0 +1,23 @@
+package com.ajeet.learnings.streaming.meetup.kafka;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/kafka")
+public class KafkaController {
+    private final KafkaProducer producer;
+
+    @Autowired
+    KafkaController(KafkaProducer producer) {
+        this.producer = producer;
+    }
+
+    @PostMapping(value = "/publish")
+    public void sendMessageToKafkaTopic(@RequestParam("message") String message) {
+        this.producer.sendMessage(message);
+    }
+}
